ba8b40f65566ad1fd944b81d0383325a718893a5
kevinkutoyi <kevinkutoyi95@gmail.com>
2022-10-01 21:48:36 +0100

    cleaning the code

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-// console.log(window);
-
 var box = document.getElementById("box");
 let firstTitle = document.getElementById("first-title");
 let offnyet = document.getElementById("offnyet");
 let container = document.getElementById("container");
 let tarehe = document.getElementById("tarehe");
-
 container.addEventListener("mousemove", (e) => {
-  //   console.log(e);
   firstTitle.style.backgroundColor =
     "rgb(" + e.offsetX + "," + e.offsetY + ",999)";
   offnyet.innerHTML =
     "<h2> OFFSETX: " + e.offsetX + "</h2><h2> OFFSETY: " + e.offsetY + "</h2>";
 });
 container.addEventListener("mousemove", (e) => {
-  //   console.log(e);
   box.style.backgroundColor = "rgb(" + e.offsetY + "," + e.offsetX + ",199)";
-  //   offnyet.innerHTML =
-  //     "<h2> OFFSETX: " + e.offsetX + "</h2><h2> OFFSETY: " + e.offsetY + "</h2>";
 });
-
-// tarehe.innerHTML = Date();
